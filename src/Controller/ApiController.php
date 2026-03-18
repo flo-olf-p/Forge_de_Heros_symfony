c37d5f5e8e54f53529f2038ca0200bd4a230a73a
Tristan Fabre <tristan.fabre17@gmail.com>
2026-03-18 12:32:25 +0100

    Ajout de l'entité User, des templates et routes de base.

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ApiController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function app_home() : Response
+    {
+        return $this->render('home.html.twig');
+    }
+    #[Route('/api', name: 'api_home')]
+    public function api_home() : Response
+    {
+        return $this->render('api/api.html.twig');
+    }
+}
