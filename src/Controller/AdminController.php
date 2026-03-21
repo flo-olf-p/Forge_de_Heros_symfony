8eef9a64034935836fec23c4953a5877652c46f4
flpleigneu <Florian.PLEIGNEUR@etu.uca.fr>
2026-03-21 16:51:07 +0100

    l'admin peut voir tout les users

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,4 +17,14 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/admin.html.twig');
     }
+
+    #[Route('/admin/allUsers', name: 'admin_allUsers')]
+    public function admin_allUsers(EntityManagerInterface $entityManager) : Response
+    {
+        $repository = $entityManager->getRepository(User::class);
+        $users = $repository->findAll();
+        return $this->render('admin/admin_allUsers.html.twig', ['users' => $users]);
+    }
+
+
 }
